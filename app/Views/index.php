1dd922ac68c96c908939e688d50c3e358002bbac
Carlos-Intecap <carlostavico2024@gmail.com>
2024-07-26 11:53:36 -0600

    Diseno mejorado

--- app/Views/index.php
+++ app/Views/index.php
@@ -10,45 +10,66 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
+<style>
+body {
+    background-image: url('https://about.dish.com/download/WIRE_26002_WirelessTower_OutdoorShoot_Hero_019.jpg');
+}
+
+.card {
+    background-color: rgba(190, 242, 255, 0.3);
+}
+
+h1 {
+    margin: 100px 0;
+}
+
+.card-body {
+    font-size: 200px;
+}
+
+.card-body:hover {
+    color: rgb(80, 172, 255);
+}
+</style>
+
 <body class="bg-success-subtle">
 
     <div class="container">
+        <div class="container text-center">
+            <h1>Telefonía</h1>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
 
-        <div class="row justify-content-center m-4">
-            <div class="col col-4 text-center">
-                <h1>Telefonía</h1>
-                <div class="list-group">
-                    <a href="clientes" class="list-group-item list-group-item-action">
-                        <div class="card">
-                            <div class="card-body">
-                                <i class="bi bi-person-fill fs-1"></i>
-                                <h5 class="card-title">Clientes</h5>
-                            </div>
+                <a href="clientes" class="col text-decoration-none">
+                    <div class="card">
+                        <div class="card-body">
+                            <i class="bi bi-person-fill titulo"></i>
+                            <h5 class="card-title">Clientes</h5>
+
                         </div>
-                    </a>
-                    <a href="lineas" class="list-group-item list-group-item-action">
-                        <div class="card">
-                            <div class="card-body">
-                                <i class="bi bi-telephone-fill fs-1"></i>
-                                <h5 class="card-title">Líneas telefónicas</h5>
-                            </div>
+                    </div>
+                </a>
+                <a href="lineas" class="col  text-decoration-none">
+                    <div class="card">
+                        <div class="card-body">
+                            <i class="bi bi-telephone-fill"></i>
+                            <h5 class="card-title">Líneas telefónicas</h5>
                         </div>
-                    </a>
-                    <a href="planes" class="list-group-item list-group-item-action">
-                        <div class="card">
-                            <div class="card-body">
-                                <i class="bi bi-menu-button-wide fs-1"></i>
-                                <h5 class="card-title">Planes</h5>
-                            </div>
+                    </div>
+                </a>
+                <a href="planes" class="col text-decoration-none">
+                    <div class="card">
+                        <div class="card-body">
+                            <i class="bi bi-menu-button-wide"></i>
+                            <h5 class="card-title">Planes</h5>
                         </div>
-                    </a>
-                </div>
+                    </div>
+                </a>
             </div>
+
         </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
